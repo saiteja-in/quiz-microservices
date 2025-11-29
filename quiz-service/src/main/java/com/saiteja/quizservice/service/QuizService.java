37f0352f341584d9cac46658dad905c81be36224
saiteja-in <vurukondasaiteja13@gmail.com>
2025-11-29 12:05:49 +0530

    removed unused dependencies

--- quiz-service/src/main/java/com/saiteja/quizservice/service/QuizService.java
+++ quiz-service/src/main/java/com/saiteja/quizservice/service/QuizService.java
@@ -10,9 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class QuizService {
